768b8fde974ed51351c839feaedc3a5adb7058cb
JeremyWarui <jerywarne7@gmail.com>
2023-09-04 02:53:57 -0700

    Starwars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+let listOfActors;
+
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(body);
+    });
+  });
+};
+
+async function displayCharacters () {
+  let movie = await requestPromise(url);
+  movie = JSON.parse(movie);
+  listOfActors = movie.characters;
+  console.log(listOfActors);
+
+  for (const actor of listOfActors) {
+    let character = await requestPromise(actor);
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
+}
+
+displayCharacters();
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,12 @@
+# 0x06. Star Wars API
+
+Interview question on using request module to fetch data from an API using nodeJS
+
+## TASK
+
+Write a script that prints all characters of a Star Wars movie:
+
+- The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+- Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+- You must use the Star wars API
+- You must use the request module
