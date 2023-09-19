18e12aeda1c2c71005fbd24d8b1b74f4fc5e3a50
JeremyWarui <jerywarne7@gmail.com>
2023-09-19 05:53:09 +0000

    Prime game answer

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""prime game"""
+
+
+def isWinner(x, nums):
+    """returns the winner"""
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    if maria_wins > ben_wins:
+        return "Ben"
+    elif maria_wins < ben_wins:
+        return "Maria"
+    else:
+        return None
---
+++ 0x0A-primegame/main_0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+isWinner = __import__('0-prime_game').isWinner
+
+
+print("Winner: {}".format(isWinner(10, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
+
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
