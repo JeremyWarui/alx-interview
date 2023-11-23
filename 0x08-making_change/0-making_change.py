8771160f6d263ae55a4dfa47a699aabc8f675020
Jeremy <jerywarne7@gmail.com>
2023-11-24 00:01:04 +0300

    Making change problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,21 +14,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    next_coin = total + 1
-    min_coins = {0: 0}
-
-    for i in range(1, total + 1):
-        min_coins[i] = next_coin
-        for coin in coins:
-            curr = i - coin
-            if curr < 0:
-                continue
-            min_coins[i] = min(min_coins[curr] + 1, min_coins[i])
-    if min_coins[total] == total + 1:
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        times = total // coin
+        num_coins += times
+        total -= coin * times
+    if total != 0:
         return -1
-    return min_coins[total]
-
-    # if min_coins[total] != float('inf') or min_coins[total] == (total + 1):
-    # return min_coins[total]
-    # else:
-    #     return -1
+    return num_coins
