684f231befc4b518d58edebb88f9b2db60cc523f
JeremyWarui <jerywarne7@gmail.com>
2023-12-14 10:15:24 +0000

    Log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,46 +12,37 @@ stdin line and checks the status code of each GET request
 Exists if interrupted by keyboard and prints the statistics
 """
 import sys
+import re
+import signal
 
 if __name__ == "__main__":
-    total_size = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-        }
-    line_cnt = 0
+    total_file_size = 0
+    status_code_cnt = {}
+    line_count = 0
 
-    def print_stats():
+    pattern = r"(\S+) - \[(.+)\] \"GET /projects/260 HTTP/1.1\" (\d+) (\d+)"
+
+    def print_metrics():
         """print the statistics of the metrix on status codes"""
-        # total_size
-        # status_codes
-        print("File size: {}".format(total_size))
-        for code in sorted(status_codes.keys()):
-            if status_codes[code]:
-                print("{}: {}".format(code, status_codes[code]))
+        global total_file_size, status_code_cnt
+        print("File size: {}".format(total_file_size))
+        for status in sorted(status_code_cnt):
+            print("{}: {}".format(status, status_code_cnt[status]))
 
     try:
-        """split the inputs in stdin into a list"""
         for line in sys.stdin:
-            list_ln = line.split()
-        if len(list_ln) >= 7 and list_ln[2].startswith("GET"):
-            status_code = int(list_ln[-2])
-            file_size = int(list_ln[-1])
-            total_size += file_size
-            """increment the count of status_code"""
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        """increment line count"""
-        if line_cnt % 10 == 0:
-            print_stats()
-        line_cnt += 1
+            line_count += 1
+            match = re.match(pattern, line)
+            if match:
+                ip, date, status, size = match.groups()
+                size = int(size)
+                total_file_size += size
+                status_code_cnt[status] = status_code_cnt.get(
+                    status, 0) + 1
+                if line_count % 10 == 0:
+                    print_metrics()
+                    line_count += 1
     except KeyboardInterrupt:
         """if keyboard interrupt: print stats and exit"""
-        print_stats()
+        print_metrics()
         sys.exit()
